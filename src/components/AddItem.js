e1ef3be78e1322671a0f118ea9a4cf70da248818
haidardzaky <haidardz@gmail.com>
2018-04-05 12:47:17 +0700

    add item js

---
+++ src/components/AddItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class AddItem extends Component {
+  render(){
+    return(
+      <div>
+        <h2>Welcome to Add Item</h2>
+      </div>
+    )
+  }
+}
