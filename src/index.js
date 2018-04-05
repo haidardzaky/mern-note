91b44072e4a97282c14c3e229d6686abd29aba4c
haidardzaky <haidardz@gmail.com>
2018-04-05 12:31:58 +0700

    edit index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowseRouter as Router, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <Router>
+    <div>
+      <Route exact path='/' component={App} />
+    </div>
+  </Router>,
+  document.getElementById('root')
+);
